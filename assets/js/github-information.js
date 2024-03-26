03f3f910f60dc2c657a1e67b213f2c9080eb1c1c
Keiran West <127797224+GitHubWestie@users.noreply.github.com>
2024-03-26 09:13:01 +0000

    added code for userinfo

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,20 @@
+function userInformationHTML(user) {
+    return `<h2>${user.name}
+                <span class="small-name">
+                    (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+                </span>
+            </h2>            
+            <div class="gh-content">
+                <div class="gh-avatar">
+                    <a href="${user.html_url}" target="_blank">
+                        <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}">
+                    </a>
+                </div>
+                <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
+            </div>`;
+
+}
+
 function fetchGitHubInformation() {
     let username = $('#gh-username').val();
     if (!username) {
@@ -19,10 +36,10 @@ function fetchGitHubInformation() {
             $('#gh-user-data').html(userInformationHTML(userData));
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
-                $('#gh-user-data').html(`<h2>No info found for user ${username}</h2>`)
+                $('#gh-user-data').html(`<h2>No info found for user ${username}</h2>`);
             } else {
                 console.log(errorResponse);
-                $('gh-user-data').html(`<h2>Error: ${errorResponse.response.JSON.message}`)
+                $('gh-user-data').html(`<h2>Error: ${errorResponse.response.JSON.message}`);
             }
         }
     );
