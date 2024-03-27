ecbfbd8b48e9bba420e7ee02405c8f4ba6bf3cec
Keiran <127797224+GitHubWestie@users.noreply.github.com>
2024-03-27 15:00:42 +0000

    Sending Emails!

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+function sendMail(contactForm) {
+    emailjs.send("service_ef80tkf","rosie", {
+        "from_name": contactForm.name.value, 
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    }) 
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            consolelog("FAILED", error);
+        });
+        return false;
+}
