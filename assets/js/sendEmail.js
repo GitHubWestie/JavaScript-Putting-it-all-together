b31dd872397c60f4238d4256a690c1990f0e98a5
Keiran <127797224+GitHubWestie@users.noreply.github.com>
2024-03-27 15:25:59 +0000

    updated message body key in .send object

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -2,7 +2,7 @@ function sendMail(contactForm) {
     emailjs.send("service_ef80tkf","rosie", {
         "from_name": contactForm.name.value, 
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "message": contactForm.projectsummary.value
     }) 
     .then(
         function(response) {
